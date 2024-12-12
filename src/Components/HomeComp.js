8d1cff7aa6331a8a68079ec599eb40386fc5697b
Narasimha191031018 <narasimhagorla45@gmail.com>
2024-12-12 08:38:40 +0530

    dummy

--- src/Components/HomeComp.js
+++ src/Components/HomeComp.js
@@ -1,111 +1,172 @@
-import React from 'react'
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import images from"./image";
-import Homecss from "./Home.module.css";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import images from './image';
+import Homecss from './Home.module.css';
+
 const HomeComp = () => {
   return (
     <div>
-        <header>
-            <div className={Homecss.stylebrand}>
-                <img className={Homecss.logo} src={images.logo} alt='logo'></img>
-                <h1 className={Homecss.brandname}>Narasimha Jewellery</h1>
-            </div>
-        </header>
-        <nav>
+      <header>
+        <div className={Homecss.stylebrand}>
+          <img className={Homecss.logo} src={images.logo} alt="Narasimha Jewellery logo" />
+          <h1 className={Homecss.brandname}>Narasimha Jewellery</h1>
+        </div>
+      </header>
+      <nav>
         <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
-  <div className="carousel-indicators">
-    <button
-      type="button"
-      data-bs-target="#carouselExampleCaptions"
-      data-bs-slide-to="0"
-      className="active"
-      aria-current="true"
-      aria-label="Slide 1"
-    ></button>
-    <button
-      type="button"
-      data-bs-target="#carouselExampleCaptions"
-      data-bs-slide-to="1"
-      aria-label="Slide 2"
-    ></button>
-    <button
-      type="button"
-      data-bs-target="#carouselExampleCaptions"
-      data-bs-slide-to="2"
-      aria-label="Slide 3"
-    ></button>
-  </div>
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      <img
-        className={Homecss.corousalImg1}
-        src={images.corousalImg1}
-        alt="Men Gold Kada"
-      />
-      <div className="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
+          {/* Carousel Indicators */}
+          <div className="carousel-indicators">
+            <button
+              type="button"
+              data-bs-target="#carouselExampleCaptions"
+              data-bs-slide-to="0"
+              className="active"
+              aria-current="true"
+              aria-label="Slide 1"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleCaptions"
+              data-bs-slide-to="1"
+              aria-label="Slide 2"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleCaptions"
+              data-bs-slide-to="2"
+              aria-label="Slide 3"
+            ></button>
+          </div>
+          {/* Carousel Inner */}
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img
+                className={`d-block w-100 ${Homecss.corousalImg1}`}
+                src={images.corousalImg1}
+                alt="Men Gold Kada"
+              />
+              <div className="carousel-caption d-none d-md-block">
+                <h5>First slide label</h5>
+                <p>Some representative placeholder content for the first slide.</p>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <img
+                className={`d-block w-100 ${Homecss.corousalImg2}`}
+                src={images.corousalImg2}
+                alt="Gold Chain"
+              />
+              <div className="carousel-caption d-none d-md-block">
+                <h5>Second slide label</h5>
+                <p>Some representative placeholder content for the second slide.</p>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <img
+                className={`d-block w-100 ${Homecss.corousalImg3}`}
+                src={images.corousalImg3}
+                alt="Silver Jewellery"
+              />
+              <div className="carousel-caption d-none d-md-block">
+                <h5>Third slide label</h5>
+                <p>Some representative placeholder content for the third slide.</p>
+              </div>
+            </div>
+          </div>
+          {/* Carousel Controls */}
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="prev"
+          >
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="next"
+          >
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+        <br />
+        {/* Additional Content */}
+        <div>
+          <div className={Homecss.flex}>
+            <img className={Homecss.rimage} src={images.rimage1} alt="Special Offers" />
+            <img className={Homecss.rimage} src={images.rimage2} alt="New Arrivals" />
+            <img className={Homecss.rimage} src={images.rimage3} alt="Coins & Bars" />
+            <img className={Homecss.rimage} src={images.rimage4} alt="Pendants" />
+            <img className={Homecss.rimage} src={images.rimage5} alt="Silver Coins" />
+            <img className={Homecss.rimage} src={images.rimage6} alt="Rings" />
+          </div>
+        </div>
+      </nav>
+      <div className='text1'>
+        <h1>Gold Jewellery</h1>
+        <h4>Embrace the golden glow this festive season</h4>
       </div>
-    </div>
-    <div className="carousel-item">
-      <img
-        className={Homecss.corousalImg2}
-        src={images.corousalImg2}
-        alt="Gold Chain"
-      />
-      <div className="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
-    </div>
-    <div className="carousel-item">
-      <img
-        className={Homecss.corousalImg3}
-        src={images.corousalImg3}
-        alt="Silver Jewellery"
-      />
-      <div className="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
-    </div>
-  </div>
-  <button
-    className="carousel-control-prev"
-    type="button"
-    data-bs-target="#carouselExampleCaptions"
-    data-bs-slide="prev"
-  >
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Previous</span>
-  </button>
-  <button
-    className="carousel-control-next"
-    type="button"
-    data-bs-target="#carouselExampleCaptions"
-    data-bs-slide="next"
-  >
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
-  </button>
-</div>
-<br></br>
-<div>
-    <div className={Homecss.flex}>
-      <img className={Homecss.rimage} src={images.rimage1} alt="offersimg"></img>
-      <img className={Homecss.rimage} src={images.rimage2} alt="NewArrivals"></img>
-      <img className={Homecss.rimage} src={images.rimage3} alt="Coins & bars"></img>
-      <img className={Homecss.rimage} src={images.rimage4} alt="pedants"></img>
-      <img className={Homecss.rimage} src={images.rimage5} alt="silvercoins"></img>
-      <img className={Homecss.rimage} src={images.rimage6} alt="ring"></img>
-      
-    </div>
-      
 
-</div>
- </nav>
+    <div className="container my-4">
+      <div className="row">
+        {/* First Row */}
+        <div className="col-md-6 mb-4">
+          <img className={`img-fluid ${Homecss.goldimg1}`} src={images.goldimg1} alt="Necklace" />
+        </div>
+        <div className="col-md-6 mb-4">
+          <img className={`img-fluid ${Homecss.goldimg2}`} src={images.goldimg2} alt="Bangles" />
+        </div>
+      </div>
+      <div className="row">
+        {/* Second Row */}
+        <div className="col-md-6 mb-4">
+          <img className={`img-fluid ${Homecss.goldimg3}`} src={images.goldimg3} alt="Rings" />
+        </div>
+        <div className="col-md-6 mb-4">
+          <img className={`img-fluid ${Homecss.goldimg4}`} src={images.goldimg4} alt="Earrings" />
+        </div>
+      </div>
     </div>
-  )
-}
+
+    <div>
+      <h1>Diamond Jewellery</h1>
+      <h4>Sparkle through the events with timeless diamonds</h4>
+    </div>
+    <div className="container my-4">
+      <div className="row">
+        {/* Left Image */}
+        <div className="col-md-4">
+          <img className={`img-fluid ${Homecss.bimg1}`} src={images.bimg1} alt="Left Image" style={{ height: '612px', objectFit: 'cover' }}/>
+        </div>
+        {/* Right Section */}
+        <div className="col-md-8">
+          {/* Top Image on Right */}
+          <div className="row mb-3">
+            <div className="col-md-12">
+              <img className={`img-fluid ${Homecss.bimg2}`} src={images.bimg2} alt="Top Right Image" />
+            </div>
+          </div>
+          {/* Bottom Images on Right */}
+          <div className="row">
+            <div className="col-md-6">
+              <img className={`img-fluid ${Homecss.bimg3}`} src={images.bimg3} alt="Bottom Left Image" />
+            </div>
+            <div className="col-md-6">
+              <img className={`img-fluid ${Homecss.bimg4}`} src={images.bimg4} alt="Bottom Right Image" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    </div>
+    
+  );
+};
+
 export default HomeComp;
--- src/Components/image.js
+++ src/Components/image.js
@@ -9,8 +9,16 @@ import rimage3 from "../Assets/Images/round3.jpg";
 import rimage4 from "../Assets/Images/round4.jpg";
 import rimage5 from "../Assets/Images/round5.jpg";
 import rimage6 from "../Assets/Images/round7.jpg";
-
+import goldimg1 from "../Assets/Images/goldimg1.jpg";
+import goldimg2 from "../Assets/Images/goldimg2.jpg";
+import goldimg3 from "../Assets/Images/goldimg3.jpg";
+import goldimg4 from "../Assets/Images/goldimg4.jpg";
+import bimg1 from "../Assets/Images/bimg1.jpg";
+import bimg2 from "../Assets/Images/bimg2.jpg";
+import bimg3 from "../Assets/Images/bimg3.jpg";
+import bimg4 from "../Assets/Images/bimg4.jpg";
 const images={
-    logo,corousalImg1,corousalImg2,corousalImg3,rimage1,rimage2,rimage3,rimage4,rimage5,rimage6
+    logo,corousalImg1,corousalImg2,corousalImg3,rimage1,rimage2,rimage3,rimage4,rimage5,rimage6,goldimg1,goldimg2,goldimg3,goldimg4,
+    bimg1,bimg2,bimg3,bimg4,
 }
 export default images;
